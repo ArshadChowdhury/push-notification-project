2d3afa87bc397603a57c6028c19e39de98ab111a
Mohammed Arshad <arshadchowdhury46@gmail.com>
2025-04-08 13:11:12 +0600

    Fixed the push queue which was failing because of not registering it in the module

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bull';
 import { PushProcessor } from './push.processor';
+import { PushNotificationModule } from 'src/push-notification/push-notification.module';
 
 @Module({
     imports: [
       BullModule.registerQueue({
         name: 'push-queue',
       }),
+      PushNotificationModule,
     ],
     providers: [PushProcessor],
     exports: [],
--- src/push-notification/push-notification.module.ts
+++ src/push-notification/push-notification.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
 import { PushNotificationController } from './push-notification.controller';
 import { PushNotificationService } from './push-notification.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [UsersModule,
+    BullModule.registerQueue({
+      name: 'push-queue', 
+    }),],
   controllers: [PushNotificationController],
-  providers: [PushNotificationService]
+  providers: [PushNotificationService],
+  exports: [PushNotificationService],
 })
 export class PushNotificationModule {}
